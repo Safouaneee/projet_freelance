24d03bbfde25b4d69dfcb2971a84d9fd195b09a7
safouane <ssafouan15@gmail.com>
2025-12-23 12:59:45 +0100

    corriger les ereur de css

--- src/gerer_les_projets_freelance/form_pr_fr.js
+++ src/gerer_les_projets_freelance/form_pr_fr.js
@@ -120,6 +120,7 @@ function Form_pr_fr(){
 
 
             }
+           
           axios.post("https://6949a6871282f890d2d6b289.mockapi.io/projets", newProjet)
                 .then(res => {
                 dispatcher(setProjects([...projets, res.data]))
